87c833e40ccd1f611ad4cfd8f93aeee036cb21f4
Luis <lfelipe.sm@gmail.com>
2024-10-13 17:31:17 +0100

    create Flappy Bird with Swing

---
+++ Flappy-Bird/src/io/codeforall/fanstatics/Util.java
@@ -0,0 +1,32 @@
+package io.codeforall.fanstatics;
+
+import javax.imageio.ImageIO;
+import java.awt.*;
+import java.io.File;
+import java.io.IOException;
+import java.util.HashMap;
+
+public class Util {
+    private static HashMap<String, Image> cache = new HashMap<String, Image>();
+
+    public static Image loadImage(String path) {
+        Image image = null;
+
+        if (cache.containsKey(path)) {
+            return cache.get(path);
+        }
+
+        try {
+            image = ImageIO.read(new File(path));
+
+            if (!cache.containsKey(path)) {
+                cache.put(path, image);
+            }
+        }
+        catch (IOException e) {
+            e.printStackTrace();
+        }
+
+        return image;
+    }
+}
