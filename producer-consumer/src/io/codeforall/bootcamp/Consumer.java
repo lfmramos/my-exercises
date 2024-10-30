e76b09c7670a0cca42ce6774c08fde7bc23540eb
Luis Ramos <lfelipe.sm@gmail.com>
2024-10-30 13:22:46 +0000

    create structure for producer-consumer exercise

---
+++ producer-consumer/src/io/codeforall/bootcamp/Consumer.java
@@ -0,0 +1,28 @@
+package io.codeforall.bootcamp;
+
+import io.codeforall.bootcamp.bqueue.Pizza;
+import io.codeforall.bootcamp.bqueue.BQueue;
+
+/**
+ * Consumer of integers from a blocking queue
+ */
+public class Consumer implements Runnable {
+
+    private final BQueue<Pizza> queue;
+    private int elementNum;
+
+    /**
+     * @param queue the blocking queue to consume elements from
+     * @param elementNum the number of elements to consume
+     */
+    public Consumer(BQueue queue, int elementNum) {
+        this.queue = queue;
+        this.elementNum = elementNum;
+    }
+
+    @Override
+    public void run() {
+
+    }
+
+}
