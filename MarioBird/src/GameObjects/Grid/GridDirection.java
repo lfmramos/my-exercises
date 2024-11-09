bc833ce0c3fe9fd927b549450ee2edfdd851c542
Luis <lfelipe.sm@gmail.com>
2024-11-09 14:54:22 +0000

    organize my Exercises repository

--- MarioBird/src/GameObjects/Grid/GridDirection.java
+++
@@ -1,5 +0,0 @@
-package GameObjects.Grid;
-
-public enum GridDirection {
-    LEFT
-}
--- MarioBird/src/Main.java
+++
@@ -1,7 +0,0 @@
-public class Main {
-
-    public static void main(String[] args) {
-        GameEngine game = new GameEngine();
-        game.start();
-    }
-}
